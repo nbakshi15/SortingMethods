3b756eaec9e44f7875ae470cda2273ae47a3a966
nbakshi15 <nirali.bakshi@yahoo.com>
2017-02-09 17:57:07 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int theArray[] = {4, 8, 15, 16, 23, 42};
+
+        sort(theArray);
+
+        for(int j = 0; j < theArray.length; j++) {
+
+            System.out.print(theArray[j] + ""); }
+
+        System.out.println(""); }
+
+    public static void sort(int a[ ]) {
+    }
+}
